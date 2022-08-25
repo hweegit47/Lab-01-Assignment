678850a9f0d88d29bb185baad76f56fdef10195d
hweegit47 <112046475+hweegit47@users.noreply.github.com>
2022-08-24 22:39:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+* Java Program Name
+*******************************************************************************
+* Raymond Hwee
+* 8/24/22
+* 255 and 004
+*******************************************************************************/
+
+public class Hello {
+    public static void (String[] args) {
+
+        System.out.println("<Raymond Hwee>");
+        System.out.println("<Hello.Java>");
+        System.out.println("<Program/Project Description>");
+        System.out.println("<8/24/22>");
+        System.out.println("<255 and 004>");
+        System.out.println();
+    }    
+}       
